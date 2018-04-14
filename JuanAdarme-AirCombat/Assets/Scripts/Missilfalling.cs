ae663588f8673ea0c5bded0a39739b4408708bfa
Mac <sebasa_t@hotmail.com>
2018-04-14 18:18:41 -0500

    Actividad 4

---
+++ JuanAdarme-AirCombat/Assets/Scripts/Missilfalling.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missilfalling : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate (0, 0, 5);
+		
+	}
+		void OnCollisionEnter(Collision collision) {
+
+			Destroy (collision.collider.gameObject);
+
+		//Se destruye el objeto impactado
+		Destroy (collision.collider.gameObject);
+
+		//Se destruye el misils
+		Destroy (this.gameObject);
+		
+	}
+}
